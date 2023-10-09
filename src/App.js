fd6944929c6810509d71c28709e3a6783dfad86b
Nosakeys <silaskeys95@gmail.com>
2023-10-09 22:44:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="h-96 w-screen flex items-center justify-center bg-red-800 text-white">
+      Hey Nosa, Let's Build!!!!!
+    </div>
+  );
+}
+
+export default App;
