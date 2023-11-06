5243c69a1b7b2bcf56ff3d88ac6d4fb3c2e60a00
Aigbe Silas <93807058+Nosakeys@users.noreply.github.com>
2023-11-06 18:05:21 +0100

    Update wow.jsx

--- src/wow.jsx
+++ src/wow.jsx
@@ -1,20 +1,3 @@
 Import React from react;
 
-const Wow
-jshdbsvsbs
-sbshsjidhdbs
-shsudushvshshsysh
-shsusihsbsbvsusos
-shsuziisjbabagaiabs
-susisjbsbs
-shsiisbavs
-shdisibsbanoabd
-shsisj s bsos
-sisosobsb s
-soshdbbsos
-shdoks bagdos
-svdiks behe
-shdikwbbsusbsvis
-shsikbdbbs
-shsokkbdbbs
-shdoisb. d
+const altWow
